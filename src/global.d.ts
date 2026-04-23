ccf72284bb3a82322aad992955bc4dca3bb7d927
JSingh950 <mtek0126@gmail.com>
2026-04-23 02:18:48 -0400

    fix: add TS declarations for model-viewer and image imports

---
+++ src/global.d.ts
@@ -0,0 +1,10 @@
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.png";
+declare module "*.JPEG";
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    "model-viewer": any;
+  }
+}
